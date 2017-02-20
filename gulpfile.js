7c03b48d96cefa39b65914d1f79b063fb921b372
Aaron Mazade <aaronm@stevensinc.com>
2017-02-20 13:17:03 -0500

    Work to do...

---
+++ gulpfile.js
@@ -0,0 +1,141 @@
+'use strict';
+
+const gulp        = require('gulp');
+const del         = require('del');
+const util        = require('gulp-util');
+const sass        = require('gulp-sass');
+const prefixer    = require('gulp-autoprefixer');
+const uglify      = require('gulp-uglify');
+const concat      = require('gulp-concat');
+const rename      = require('gulp-rename');
+const handlebars  = require('gulp-compile-handlebars');
+const browserSync = require('browser-sync');
+const ghPages     = require('gulp-gh-pages');
+const sassGlob    = require('gulp-sass-bulk-import');
+const watch       = require('gulp-watch');
+const babel       = require('gulp-babel');
+
+var paths = {
+  src: { root: 'src' },
+  dist: { root: 'dist' },
+  init: function() {
+    this.src.sass        = this.src.root + '/public/scss/docs.scss';
+    this.src.templates   = this.src.root + '/**/*.hbs';
+    this.src.javascript  = [this.src.root + '/public/js/**/*.js', '!' + this.src.root + '/js/libs/*.js'];
+    this.src.libs        = this.src.root + '/public/js/libs/*.js';
+    this.src.images      = this.src.root + '/public/img/**/*.{jpg,jpeg,svg,png,gif}';
+    this.src.files       = this.src.root + '/*.{html,txt}';
+
+    this.dist.css        = this.dist.root + '/css';
+    this.dist.images     = this.dist.root + '/img';
+    this.dist.javascript = this.dist.root + '/js';
+    this.dist.libs       = this.dist.root + '/js/libs';
+
+    return this;
+  },
+}.init();
+
+gulp.task('serve', () => {
+  browserSync.init({
+    server: paths.dist.root,
+    open: false,
+    notify: false,
+
+    // Whether to listen on external
+    online: false,
+  });
+});
+
+gulp.task('styles', () => {
+  gulp.src([paths.src.sass])
+    .pipe(sassGlob())
+    .on('error', util.log)
+    .pipe(sass({
+      includePaths: ['src/scss'],
+    }))
+    .on('error', util.log)
+    .pipe(prefixer('last 4 versions'))
+    .on('error', util.log)
+    .pipe(gulp.dest(paths.dist.css))
+    .pipe(browserSync.reload({stream: true}));
+});
+
+/*
+* Compile handlebars/partials into html
+*/
+gulp.task('templates', () => {
+  var opts = {
+    ignorePartials: true,
+    batch: ['src/partials'],
+  };
+
+  gulp.src([paths.src.root + '/*.hbs'])
+    .pipe(handlebars(null, opts))
+    .on('error', util.log)
+    .pipe(rename({
+      extname: '.html',
+    }))
+    .on('error', util.log)
+    .pipe(gulp.dest(paths.dist.root))
+    .pipe(browserSync.reload({stream: true}));
+});
+
+/*
+* Bundle all javascript files
+*/
+gulp.task('scripts', () => {
+  gulp.src(paths.src.javascript)
+    .pipe(babel({
+      presets: ['es2015'],
+    }))
+    .pipe(concat('bundle.js'))
+    .on('error', util.log)
+    .pipe(uglify())
+    .on('error', util.log)
+    .pipe(gulp.dest(paths.dist.javascript))
+    .pipe(browserSync.reload({stream: true}));
+
+  /*
+  * Uglify JS libs and move to dist folder
+  */
+  gulp.src([paths.src.libs])
+    .pipe(uglify())
+    .on('error', util.log)
+    .pipe(rename({
+      suffix: '.min',
+    }))
+    .on('error', util.log)
+    .pipe(gulp.dest(paths.dist.libs))
+    .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('images', () => {
+  gulp.src([paths.src.images])
+    .pipe(gulp.dest(paths.dist.images));
+});
+
+gulp.task('files', () => {
+  gulp.src([paths.src.files])
+    .pipe(gulp.dest(paths.dist.root));
+});
+
+watch(paths.src.images, () => {
+  gulp.start('images');
+});
+
+watch(paths.src.files, () => {
+  gulp.start('files');
+});
+
+gulp.task('watch', () => {
+  gulp.watch('src/scss/**/*.scss', ['styles']);
+  gulp.watch(paths.src.javascript, ['scripts']);
+  gulp.watch(paths.src.templates, ['templates']);
+});
+
+gulp.task('deploy', () => {
+  return gulp.src([paths.dist.root + '/**/*'])
+    .pipe(ghPages());
+});
+
+gulp.task('default', ['watch', 'serve', 'images', 'files', 'styles', 'scripts', 'templates']);
